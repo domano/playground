6550a42b6a1d300909a65aaf610384c044a2892a
Dino Omanovic <angor.mail+github@googlemail.com>
2021-04-12 12:37:05 -0700

    cobra boilerplate

--- go/gitserve/main.go
+++ go/gitserve/main.go
@@ -1,87 +1,22 @@
+/*
+Copyright © 2021 NAME HERE <EMAIL ADDRESS>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
-import (
-	"fmt"
-	"github.com/go-git/go-billy/v5"
-	"github.com/go-git/go-billy/v5/memfs"
-	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/storage/memory"
-	"io/fs"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-)
+import "github.com/domano/playground/go/gitserve/cmd"
 
 func main() {
-	// Listen for SIGINT and SIGTERM
-	sigs := make(chan os.Signal, 1)
-	done := make(chan bool, 1)
-
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
-
-	go func() {
-		sig := <-sigs
-		fmt.Println()
-		fmt.Println(sig)
-		done <- true
-	}()
-
-	storage := memory.NewStorage()
-	fileSystem := memfs.New()
-	_, err := git.Clone(storage, fileSystem, &git.CloneOptions{
-		URL:      "https://github.com/domano/playground",
-		Progress: os.Stdout,
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	fileServer := http.FileServer(NewFileSystemConnector(fileSystem))
-
-	if err := http.ListenAndServe(":8080", fileServer); err != nil {
-		panic(err)
-	}
-
-	<-done
-
-}
-
-type FileSystemConnector struct {
-	mfs billy.Filesystem
-}
-
-func NewFileSystemConnector(mfs billy.Filesystem) FileSystemConnector {
-	return FileSystemConnector{
-		mfs: mfs,
-	}
-}
-
-func (f FileSystemConnector) Open(name string) (http.File, error) {
-	if name == "/" {
-		name = "index.html"
-	} else {
-		valid := fs.ValidPath(name)
-		if !valid {
-			return nil, fs.ErrNotExist
-		}
-	}
-	file, err := f.mfs.Open(name)
-	return FileConnector{f.mfs, file, name}, err
-}
-
-type FileConnector struct {
-	billy.Filesystem
-	billy.File
-	string
-}
-
-// Readdir reads all contents regardless of count given, due to the underlying billy.Filesystem. It will not return more then count slice entries.
-func (f FileConnector) Readdir(count int) ([]fs.FileInfo, error) {
-	fi, err := f.Filesystem.ReadDir(f.string)
-	return fi[0:count], err // only return an array with as many elements as in "count"
-}
-
-func (f FileConnector) Stat() (fs.FileInfo, error) {
-	return f.Filesystem.Stat(f.Name())
+	cmd.Execute()
 }
